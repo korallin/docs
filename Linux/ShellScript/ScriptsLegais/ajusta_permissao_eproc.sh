2948800a54a6034af93f1cfc0aac0b4a90aabb29
Alexander Moreira de Morais <alex.moreira@gmail.com>
2020-10-26 16:09:18 -0300

    Script para ajuste de permissao de execucao nos arquivos do e-Proc

--- Linux/ShellScript/ScriptsLegais/ajusta_permissao_eproc.sh
+++ Linux/ShellScript/ScriptsLegais/ajusta_permissao_eproc.sh
@@ -2,6 +2,7 @@
 
 # Arquivo de Log de execucao do script
 LOG=/opt/scripts/ajusta_permissao_pet.log
+PID=/opt/scripts/ajusta_permissao_pet.pid
 
 # Lista de localidades que nao precisa executar ajuste
 lista=( "/mnt/peticao/docs/AC" "/mnt/peticao/docs/AM" "/mnt/peticao/docs/AP" "/mnt/peticao/docs/BA" "/mnt/peticao/docs/GO" "/mnt/peticao/docs/MT" "/mnt/peticao/docs/PA" "/mnt/peticao/docs/MG" "/mnt/peticao/docs/MA" "/mnt/peticao/docs/PI" "/mnt/peticao/docs/RO" "/mnt/peticao/docs/RR" "/mnt/peticao/docs/TO" )
@@ -19,8 +20,10 @@ verifica() {
 }
 
 # Verifica se o script está em execucao
-if [ `ps aux|grep -v grep| grep -c "/opt/scripts/ajusta_permissao_pet.sh"` -eq 0 ]; then
-    data=`date`
+#if [ `ps aux|grep "/opt/scripts/ajusta_permissao_pet.sh"|grep -v grep|wc -l` -lt 1 ]; then
+if [ ! -f $PID ]; then
+    echo $$ > $PID
+    data=`date +"%d/%m/%Y %H:%M:%S"`
     echo "Inicio: $data" >> $LOG
     for dir in $(ls /mnt/peticao/docs/* -d); do
         verifica $dir
@@ -28,13 +31,11 @@ if [ `ps aux|grep -v grep| grep -c "/opt/scripts/ajusta_permissao_pet.sh"` -eq 0
         if [ $verifica_retorno -eq 1 ]; then
             # Verifica se o find esta em execucao na pasta
             if [ `ps aux | grep "find $dir" | grep 0755 | wc -l` -eq 0 ]; then
-                find $dir -type f -not -perm 0755 -mtime -1 -exec chmod 755{} \;
+                find $dir -type f -not -perm 0755 -mtime -1 -exec chmod 755 {} \;
             fi
         fi
     done
-    data=`date`
+    data=`date +"%d/%m/%Y %H:%M:%S"`
     echo "Fim...: $data" >> $LOG
+    rm -rf $PID
 fi
-
-
-
