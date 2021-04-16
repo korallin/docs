7c4e182873fc2f8ac2ce6bb0bda32e598315ba83
Alexander Moreira de Morais (TR301005) <alexander.morais@trf1.jus.br>
2021-04-16 14:46:14 -0300

    Ajuste script para capturar Load

--- Linux/SNMP/monitoramento.sh
+++ Linux/SNMP/monitoramento.sh
@@ -5,12 +5,13 @@ else
 	HOST=srvpjehmlspb01.trf1.gov.br
 fi
 
+SNMP_LOAD_1MIN=.1.3.6.1.4.1.2021.10.1.3.1
 SNMP_USO_CPU=.1.3.6.1.4.1.2021.11.10.0
 SNMP_TOTAL_MEM=.1.3.6.1.4.1.2021.4.5.0
 SNMP_USO_MEM=.1.3.6.1.4.1.2021.4.6.0
 
 while true; do
-	CPU=`snmpget -c public $HOST -v 2c $SNMP_USO_CPU | cut -d' ' -f4` 
+	CPU=`snmpget -c public $HOST -v 2c $SNMP_LOAD_1MIN | cut -d' ' -f4` 
 	MEM=`snmpget -c public $HOST -v 2c $SNMP_USO_MEM | cut -d' ' -f4` 
 	DATA=`date +'%Y-%m-%d %H:%M:%S'`
 	echo "$DATA; $HOST; $CPU; $MEM"
