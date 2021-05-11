12fc83a91b1dfe7505ee0d586abe6ed92e6b146c
Alexander Moreira de Morais <alex.moreira@gmail.com>
2021-05-11 14:51:37 -0300

    Ajustes coleta de métricas

--- Linux/SNMP/monitoramento.sh
+++ Linux/SNMP/monitoramento.sh
@@ -6,12 +6,15 @@ else
 fi
 
 SNMP_USO_CPU=.1.3.6.1.4.1.2021.11.10.0
-SNMP_TOTAL_MEM=.1.3.6.1.4.1.2021.4.5.0
-SNMP_USO_MEM=.1.3.6.1.4.1.2021.4.6.0
+SNMP_MEM_TOTAL=.1.3.6.1.4.1.2021.4.5.0
+SNMP_MEM_FREE=.1.3.6.1.4.1.2021.4.6.0
 
+echo "DATA; HOST; CPU LOAD; MEM USED(MB)"
 while true; do
-	CPU=`snmpget -c public $HOST -v 2c $SNMP_USO_CPU | cut -d' ' -f4` 
-	MEM=`snmpget -c public $HOST -v 2c $SNMP_USO_MEM | cut -d' ' -f4` 
+	CPU=`snmpget -c public $HOST -v 2c $SNMP_USO_CPU | cut -d' ' -f4`
+	memTot=`snmpget -c public srvpjehmlminio1 -v 2c $SNMP_MEM_TOTAL | cut -d' ' -f4`
+	memFree=`snmpget -c public srvpjehmlminio1 -v 2c $SNMP_MEM_FREE | cut -d' ' -f4`
+	MEM=`echo "($memTot - $memFree) / 1024" |bc`
 	DATA=`date +'%Y-%m-%d %H:%M:%S'`
 	echo "$DATA; $HOST; $CPU; $MEM"
 	sleep 5
