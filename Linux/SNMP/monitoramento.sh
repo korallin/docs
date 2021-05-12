4c4e0328965a87cc602700a8f8267034ee94cee0
Alexander Moreira de Morais <alex.moreira@gmail.com>
2021-05-12 13:29:33 -0300

    Ajustes captura de Load 1min

--- Linux/SNMP/monitoramento.sh
+++ Linux/SNMP/monitoramento.sh
@@ -12,7 +12,7 @@ SNMP_MEM_FREE=.1.3.6.1.4.1.2021.4.6.0
 
 echo "DATA; HOST; CPU LOAD; MEM USED(MB)"
 while true; do
-	CPU=`snmpget -c public $HOST -v 2c $SNMP_USO_CPU | cut -d' ' -f4`
+	CPU=`snmpget -c public $HOST -v 2c $SNMP_LOAD_1MIN | cut -d' ' -f4`
 	memTot=`snmpget -c public srvpjehmlminio1 -v 2c $SNMP_MEM_TOTAL | cut -d' ' -f4`
 	memFree=`snmpget -c public srvpjehmlminio1 -v 2c $SNMP_MEM_FREE | cut -d' ' -f4`
 	MEM=`echo "($memTot - $memFree) / 1024" |bc`
